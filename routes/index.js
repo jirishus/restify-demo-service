77361898d07bfed146aa1680c659e6cf068efcbe
jirishus <jirishus@yahoo.com>
2018-08-17 20:59:59 -0700

    dynamic resitfy routes

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+/**
+ * Lets auto-magically register all the routes
+ */
+const fs = require('fs');
+
+module.exports = function (server) {
+	fs.readdirSync('./routes').forEach((file) => {
+		if (file.substr(-3, 3) === '.js' && file !== 'index.js') {
+			require(`./${ file.replace('.js', '') }`)(server);
+		}
+	});
+};
+
+/**
+ * Public API
+ */
+// module.exports = {
+	// some clever pattern here
+// }
