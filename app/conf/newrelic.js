027789c0ebd1d7ddd57d4b98b0b30fcead492576
jirishus <jirishus@yahoo.com>
2018-10-21 11:46:28 -0700

    dev stage prod gulp tasks

--- app/conf/newrelic.js
+++ app/conf/newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['restify-demo-service'],
+  app_name: ['$NEWRELIC_APP_NAME'],
   /**
    * Your New Relic license key.
    */
