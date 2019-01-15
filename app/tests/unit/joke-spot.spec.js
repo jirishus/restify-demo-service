e4e7ba2eea99288ecd24107cfd0894422c20a78b
jirishus <jirishus@yahoo.com>
2019-01-15 00:33:26 -0800

    added chai

--- app/tests/unit/joke-spot.spec.js
+++ app/tests/unit/joke-spot.spec.js
@@ -2,6 +2,9 @@ const assert = require('assert');
 const jokeSpot = require('../../providers/joke-spot');
 const nock = require('nock');
 
+const chai = require('chai');
+const expect = chai.expect;
+
 nock('https://official-joke-api.appspot.com')
  .get('/random_joke')
  .reply(200, { punchline: 'something funny' });
@@ -11,6 +14,8 @@ describe('Main Canary Test Suite', () => {
 	it('should return a 200', (done) => {
 		jokeSpot.fetchJoke()
 			.then((payload) => {
+				console.log('CHECK THIS',payload);
+				expect(payload.msg.punchline).to.equal('something funny');
 				done();
 			})
 			.catch((err) => {
